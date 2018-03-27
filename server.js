58d7beabfd52b32f6072870442d2fc8624fd7590
arjunbhexaware <37321509+arjunbhexaware@users.noreply.github.com>
2018-03-27 12:53:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 app.post('/',function(req,res){
    /* Creating a service request */
+   console.log(req.body.originalRequest.source);
  if(req.body.originalRequest.source == 'facebook'){
   fbBot.fbWebHookResponse(req,res);
 }else{
