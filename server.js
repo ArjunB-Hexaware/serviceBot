acf603b2d78e0068c0d9f9de7e9e2351598128bd
arjunbhexaware <37321509+arjunbhexaware@users.noreply.github.com>
2018-04-09 15:25:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var client = new Client(options_auth);
 var serverPort=process.env.PORT || 3000;
 var fbBot= require('./facebook-bot.js');
 var slBot= require('./slack-bot.js');
+var gAsstBot= require('./gAsst-bot');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
@@ -15,11 +16,16 @@ app.use(bodyParser.json());
 app.post('/',function(req,res){
    /* Creating a service request */
    console.log(req.body.originalRequest.source);
- if(req.body.originalRequest.source == 'facebook' || req.body.originalRequest.source == 'google'){
+ if(req.body.originalRequest.source == 'facebook'){
   fbBot.fbWebHookResponse(req,res);
 }else{
   if(req.body.originalRequest.source == 'slack_testbot'){
     slBot.slackWebHookResponse(req,res);
+  }else{
+    if(req.body.originalRequest.source == 'google'){
+      console.log("Google assistant");
+      gAsstBot.gAsstWebHookResponse(req,res);
+    }
   }
 }
 });
