2b0ff7e619dcaf05895ab2d6c64233bc83977ba6
arjunbhexaware <37321509+arjunbhexaware@users.noreply.github.com>
2018-04-09 10:35:55 +0530

    Add files via upload

--- slack-bot.js
+++ slack-bot.js
@@ -228,7 +228,7 @@ exports.slackWebHookResponse = function(req,res){
         }else{
         if(req.body.result.metadata.intentName=='serviceNowGenerateId'){
             console.log("Incident priority check condition");
-        return res.json({
+       /* return res.json({
             "messages": [
                 {
                   "platform": "slack",
@@ -241,7 +241,57 @@ exports.slackWebHookResponse = function(req,res){
                   "type": 2
                 }
               ]
-              });
+              });*/
+           return res.json({
+                "text": "Would you like to play a game?",
+                "response_type": "in_channel",
+                "attachments": [
+                    {
+                        "text": "Choose a game to play",
+                        "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
+                        "color": "#3AA3E3",
+                        "attachment_type": "default",
+                        "callback_id": "game_selection",
+                        "actions": [
+                            {
+                                "name": "games_list",
+                                "text": "Pick a game...",
+                                "type": "select",
+                                "options": [
+                                    {
+                                        "text": "Hearts",
+                                        "value": "hearts"
+                                    },
+                                    {
+                                        "text": "Bridge",
+                                        "value": "bridge"
+                                    },
+                                    {
+                                        "text": "Checkers",
+                                        "value": "checkers"
+                                    },
+                                    {
+                                        "text": "Chess",
+                                        "value": "chess"
+                                    },
+                                    {
+                                        "text": "Poker",
+                                        "value": "poker"
+                                    },
+                                    {
+                                        "text": "Falken's Maze",
+                                        "value": "maze"
+                                    },
+                                    {
+                                        "text": "Global Thermonuclear War",
+                                        "value": "war"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            })
         }else{
             if(req.body.result.metadata.intentName =='serviceNowGetDetails'){
                 console.log("servicenowgetdetails");
